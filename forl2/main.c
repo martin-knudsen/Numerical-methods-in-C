eda12312a6bc643d504f335fbefc514b5c2bf165
Unknown <martin.knudsen@protonmail.com>
2018-02-01 12:02:08 +0100

    Commit af 2 første exercise

---
+++ forl2/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int x=10;
+	x=10.1;
+	int y=3/2;	
+	printf("your number is, y= %i,2 gange x er %i, 22 gange x er %i \n", 1/2,2*x,22*x);
+	printf("%i\n",y);
+	return 0;
+}
