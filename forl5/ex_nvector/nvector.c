daae6a9c1668c973b0eccb4f902736eeedef85e6
Unknown <martin.knudsen@protonmail.com>
2018-02-14 10:27:54 +0100

    Start ex_nvector

---
+++ forl5/ex_nvector/nvector.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "nvector.h"
+
+nvector* nvector_alloc(int n){
+  nvector* v = malloc(sizeof(nvector));
+  (*v).size = n;
+  (*v).data = malloc(n*sizeof(double));
+  if( v==NULL ) fprintf(stderr,"error in nvector_alloc\n");
+  return v;
+}
+
+void nvector_free(nvector* v){ free(v->data); free(v);}
+
+void nvector_set(nvector* v, int i, double value){ (*v).data[i]=value; }
+
+double nvector_get(nvector* v, int i){return (*v).data[i]; }
+
+/* ... */
--- forl5/questions.c
+++ forl5/questions.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main() {
+	struct vector {double x,y,z;};
+	struct vector v = {1,2,3};
+	struct vector x = {1,2,3};
+	struct vector z = {1,2,3};
+	struct vector u = {.x=1,.y=2,.z=3};
+	struct vector w = {.z=3,.y=2,.x=1};
 	
+	typedef struct vector vector;
+	vector o;
+
+  	double* a=foo(); a[2]=1;
+  	double* b=foo(); b[2]=2;
+  	printf("a[2] = %g\n",a[2]);
+	return EXIT_SUCCESS;
 }
+
+double* foo(){ double a[]={0,0,0}; return &a; }
