15b38c66def1b32e54e77dc0ca837025862d093b
Unknown <martin.knudsen@protonmail.com>
2018-02-23 18:12:42 +0100

    Start lineær eq solver

--- forl7/ex_GSL/Airy_stdio.c
+++ forl7/ex_GSL/Airy_stdio.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <gsl/gsl_sf_airy.h>
 
-int () {
+int main() {
 
 	double x;
 	while(scanf("%lg",&x) != EOF) {
-		double Ai = gsl_sf_airy_Ai(x);
-		double Bi = gsl_sf_airy_Bi(x);
+		double Ai = gsl_sf_airy_Ai(x, GSL_PREC_DOUBLE);
+		double Bi = gsl_sf_airy_Bi(x, GSL_PREC_DOUBLE);
 		printf("%lg \t %g \t %g\n", x, Ai, Bi);
 	}
 	return 0;
---
+++ forl7/ex_GSL/lin_eq_solver_gsl.c
@@ -0,0 +1,3 @@
+#include <stdio.h>
+#include <gsl/gsl_linalg_HH.h>
+
