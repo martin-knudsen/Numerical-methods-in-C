2ad2ae0ca05d19c062c3b27df60d989de06d8f30
Unknown <martin.knudsen@protonmail.com>
2018-03-06 20:26:10 +0100

    begin ex_multiroot

--- forl10/questions.c
+++ forl10/questions.c
@@ -1,8 +1,8 @@
-#include"stdio.h"
-#include"math.h"
-double f(double x){return  A*exp(-x/T)+B;}
-int main(){
-	double A=3.5, T=3.2, B=1.2;
-	for(double t=0;t<=5;t+=1) printf("%g %g\n",t,f(t));
-return 0;
-}
+#include <stdio.h>
+
+
+int main( int argc, char *argv[] )  {
+
+	printf(argv[1]);
+	return 0;
+   }
