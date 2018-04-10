a739dc8256bf0e40ff9fce6750fb5d2f2c7a7fa7
Unknown <martin.knudsen@protonmail.com>
2018-04-10 17:32:38 +0200

    endnu mere done

---
+++ Homeworks/Numerical Methods/Linear Equations/QR_Gram_Schmidt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <assert.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h>
+
+
+
+int main() {
+	
+	
+	
+	return EXIT_SUCCESS;
+}
