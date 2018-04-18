f28d50f4e75cb51d4fb2d74968f6833e6a37def6
Unknown <martin.knudsen@protonmail.com>
2018-04-18 21:48:46 +0200

    B completely done

--- Homeworks/Numerical Methods/Root Finding/main.c
+++ Homeworks/Numerical Methods/Root Finding/main.c
@@ -7,6 +7,7 @@
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_blas.h>
 #include "Newton.h"
+#include <gsl/gsl_multiroots.h>
 #define RND (double)rand()/RAND_MAX
 #define FMT "%7.8f" //format of print "7 width, 3 digits after comma" 
 
@@ -93,6 +94,39 @@ int main() {
 	gsl_vector_set(fx,1,fx1);
 	function_calls++;
 	}
+
+	int system_f_gsl(const gsl_vector* v, void *params, gsl_vector* fx){
+	double A = 10000.0;
+	double x = gsl_vector_get(v,0);
+	double y =	gsl_vector_get(v,1);
+	double fx0 = A*x*y -1; 
+	double fx1 = exp(-x) + exp(-y) -(1.0 + 1.0/A);
+	gsl_vector_set(fx,0,fx0);
+	gsl_vector_set(fx,1,fx1);
+	return GSL_SUCCESS;
+	}
+
+	int Rosenbrock_grad_f_gsl(const gsl_vector* v, void *params, gsl_vector* fx){
+	double x = gsl_vector_get(v,0);
+	double y =	gsl_vector_get(v,1);
+	double fx0 = 2*x-2+400*(x*x*x-y*x); 
+	double fx1 = 200*(y-x*x);
+	gsl_vector_set(fx,0,fx0);
+	gsl_vector_set(fx,1,fx1);
+	function_calls++;
+	return GSL_SUCCESS;
+	}
+
+	int Himmelblau_grad_f_gsl(const gsl_vector* v, void *params, gsl_vector* fx){
+	double x = gsl_vector_get(v,0);
+	double y =	gsl_vector_get(v,1);
+	double fx0 = 4*x*(x*x+y-11)+2*(x+y*y-7); 
+	double fx1 = 2*(x*x+y-11)+4*y*(x+y*y-7);
+	gsl_vector_set(fx,0,fx0);
+	gsl_vector_set(fx,1,fx1);
+	function_calls++;
+	return GSL_SUCCESS;
+	}
 	
 	
 	// system
@@ -118,7 +152,7 @@ int main() {
 	printf("\n");
 	// Rosenbrock
 	function_calls = 0;
-	dx=1e-6, epsilon=1e-3;
+	dx=1e-8, epsilon=1e-3;
 	printf("Testing if the implementation works on Rosenbrock.\n");
 	gsl_vector_set(x,0,0);
 	gsl_vector_set(x,1,0);
@@ -163,7 +197,7 @@ int main() {
 
 	// system
 	function_calls = 0;
-	dx=1e-6, epsilon=1e-3;
+	dx=1e-9, epsilon=1e-3;
 	printf("Testing if the implementation works on the system of equations provided.\n");
 	
 	gsl_vector_set(x,0,1);
@@ -222,30 +256,141 @@ int main() {
 	Himmelblau_grad_f(x,fx,J);
 	printv(fx);
 	printf("Number of function calls: %i\n",function_calls);
-	
 	printf("\n");
-	printf("As one can see the use of an analytical jacobian\n
-		does not have a dramatic effect on the stepsize\n
-		and even increases it a bit in the Rosenbrock function. \n
-		This could be a coincidence as a slightly wrong jacobian\n
-		might luckily force the jacobian in a good direction of\n
-		function space maybe to not get stuck in a bad local place\n
-		Probably if we would have more variables it would matter more\n
-		However the analytical version is superior everywhere in terms\n
-		of speed as it limits the number of calls to the function significantly\n
-		which it uses 2-3 times less.
-		");
+	// Gsl stuff
+	printf("Testing with gsl multiroot fsolver using dnewton type\n");
+	int i=0, status;
+	// system
+	printf("System of equations\n");
+	dx=1e-6, epsilon=1e-3;	
+	gsl_vector_set(x,0,1);
+	gsl_vector_set(x,1,0);
+	
+	printf("xstart is:\n");
+	printv(x);	
+	
+	const gsl_multiroot_fsolver_type* T =gsl_multiroot_fsolver_dnewton;
+	gsl_multiroot_fsolver* s = gsl_multiroot_fsolver_alloc(T, n);
+		
+	gsl_multiroot_function gslsystem = {&system_f_gsl,n,NULL};
+	
+	gsl_multiroot_function gsl_Rosenbrock = {&Rosenbrock_grad_f_gsl,n,NULL};
+	gsl_multiroot_function gsl_Himmelblau = {&Himmelblau_grad_f_gsl,n,NULL};
+	
+	gsl_multiroot_fsolver_set (s,&gslsystem,x);
+	
+	
+	do{
+      i++;
+      status = gsl_multiroot_fsolver_iterate (s);
 
+      if (status) break;
+
+      status = gsl_multiroot_test_residual (s->f, epsilon);
+    }
+    while (status == GSL_CONTINUE && i < 1000);
+    
+    printf ("status = %s\n", gsl_strerror (status));
+    printf("The root found is: \n");
+	printv(s->x);
+	printf("Here f(x) is \n");
+	system_f(s->x,fx,J);
+	printv(fx);
+	printf("Number of steps taken is: %i\n", i);
+  
+
+	printf("\n");
+
+	// Rosenbrock
+	printf("Rosenbrock\n");
+	i=0;
+	dx=1e-6, epsilon=1e-3;	
+	gsl_vector_set(x,0,0);
+	gsl_vector_set(x,1,0);
+	
+	printf("xstart is:\n");
+	printv(x);	
+	gsl_multiroot_fsolver_set (s,&gsl_Rosenbrock,x);
+	
+	
+	do{
+      i++;
+      status = gsl_multiroot_fsolver_iterate (s);
+
+      if (status) break;
+
+      status = gsl_multiroot_test_residual (s->f, epsilon);
+    }
+    while (status == GSL_CONTINUE && i < 1000);
+    
+    printf ("status = %s\n", gsl_strerror (status));
+    printf("The root found is: \n");
+	printv(s->x);
+	printf("Here f(x) is \n");
+	Rosenbrock_grad_f(s->x,fx,J);
+	printv(fx);
+	printf("Number of steps taken is: %i\n", i);
+  
+
+	printf("\n");
+
+	// Himmelblau
+	printf("Himmelblau\n");
+	i=0;
+	dx=1e-6, epsilon=1e-3;	
+	gsl_vector_set(x,0,0);
+	gsl_vector_set(x,1,0);
+	
+	printf("xstart is:\n");
+	printv(x);	
+	gsl_multiroot_fsolver_set (s,&gsl_Himmelblau,x);
+	
+	
+	do{
+      i++;
+      status = gsl_multiroot_fsolver_iterate (s);
+
+      if (status) break;
+
+      status = gsl_multiroot_test_residual (s->f, epsilon);
+    }
+    while (status == GSL_CONTINUE && i < 1000);
+    
+    printf ("status = %s\n", gsl_strerror (status));
+    printf("The root found is: \n");
+	printv(s->x);
+	printf("Here f(x) is \n");
+	Himmelblau_grad_f(s->x,fx,J);
+	printv(fx);
+	printf("Number of steps taken is: %i\n", i);
+  
+
+	printf("\n");
+
+	printf("As one can see the use of an analytical jacobian\n"
+		"does not have a dramatic effect on the stepsize\n"
+		"and even increases it a bit in the Rosenbrock function. \n"
+		"This could be a coincidence as a slightly wrong jacobian\n"
+		"might luckily force the jacobian in a good direction of\n"
+		"function space maybe to not get stuck in a bad local place\n"
+		"Probably if we would have more variables it would matter more\n"
+		"However the analytical version is superior everywhere in terms\n"
+		"of speed as it limits the number of calls to the function significantly\n"
+		"which it uses 2-3 times less.\n"
+		"When comparing to the gsl method one can see that our version takes \n"
+		"many more steps. The algorithm used for gsl was the newton method without\n"
+		"analytical jacobian. The disparity must come from an optimized algorithm in gsl.\n");
 
 	printf("\n\n");
 
-
 	//part C
-	printf("C. Advanced backtracking");
+	printf("C. Advanced backtracking\n");
 
 	gsl_vector_free(x);
 	gsl_vector_free(fx);
 	gsl_matrix_free(J);
+    gsl_multiroot_fsolver_free (s);
+		
 
 	return EXIT_SUCCESS;
 }
