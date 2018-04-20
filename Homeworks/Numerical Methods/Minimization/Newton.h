dec48db4d4824db787506a1822d79f8b74773ad9
Unknown <martin.knudsen@protonmail.com>
2018-04-20 12:43:53 +0200

    B completely done

--- Homeworks/Numerical Methods/Minimization/Newton.h
+++ Homeworks/Numerical Methods/Minimization/Newton.h
@@ -1,6 +1,9 @@
 #ifndef HAVE_Newton
 #define HAVE_Newton
 
+void gradient_num(void f(gsl_vector* x, gsl_vector* fx),
+				  gsl_vector* x, gsl_vector* df, double dx);
+
 void newton_minimization(double f(gsl_vector* x, gsl_vector* fx, gsl_matrix* J),
 	gsl_vector* xstart,
 	double dx,
@@ -12,6 +15,13 @@ void newton_minimization_Broyden(
 	gsl_vector* x,
 	double dx,
 	double epsilon
-)
+);
+
+void newton_minimization_Broyden_num_gradient(
+	double f(gsl_vector* x),
+	gsl_vector* x,
+	double dx,
+	double epsilon
+);
 
 #endif
--- Homeworks/Numerical Methods/Minimization/main.c
+++ Homeworks/Numerical Methods/Minimization/main.c
@@ -7,13 +7,12 @@
 #include <gsl/gsl_vector.h>
 #include <gsl/gsl_blas.h>
 #include "Newton.h"
-#include <gsl/gsl_multiroots.h>
 #define RND (double)rand()/RAND_MAX
 #define FMT "%7.8f" //format of print "7 width, 3 digits after comma" 
 
 
 // inspired by Dmitri Fedorovs print implementation
-void printm(gsl_matrix *A){
+void printm(gsl_matrix* A) {
 	// iterating over all rows and columns 
 	for(int i=0;i<A->size1;i++){
 		for(int j=0;j<A->size2;j++) {
@@ -29,11 +28,11 @@ void printv(gsl_vector *A){
 	}
 }
 
-int main() {
+int main(void) {
 	// part A
 	int n=2;
 	int function_calls = 0;
-	double epsilon=1e-3, dx=1e-3;
+	double epsilon=1e-3, dx=1e-6;
 
 	gsl_vector* x = gsl_vector_alloc(n);
 	gsl_vector* df = gsl_vector_alloc(n);
@@ -102,6 +101,22 @@ int main() {
 	function_calls++;
 	return fx;
 	}
+
+	double master_fit(gsl_vector* v){
+	double A = gsl_vector_get(v,0);
+	double B =	gsl_vector_get(v,1);
+	double T =	gsl_vector_get(v,2);
+	double t[] = {0.23,1.29,2.35,3.41,4.47,5.53,6.59,7.65,8.71,9.77};
+	double y[] = {4.64,3.38,3.01,2.55,2.29,1.67,1.59,1.69,1.38,1.46};
+	double e[] = {0.42,0.37,0.34,0.31,0.29,0.27,0.26,0.25,0.24,0.24};
+	int N = sizeof(t)/sizeof(t[0]);
+	double fx = 0;
+	for(int i=0;i<N;i++){
+		fx +=pow((A*exp(-t[i]/T)+B-y[i])/e[i],2);
+	}
+	function_calls++;
+	return fx;
+	}
 	
 	printf("A. Newton minimization\n");
 	
@@ -119,7 +134,7 @@ int main() {
 
 	printf("The root found is: \n");
 	printv(x);
-	printf("Here f(x) is\n");
+	printf("Here df(x) is\n");
 	Rosenbrock_f(x,df,H);
 	printv(df);
 	printf("Number of function calls: %i\n",function_calls);
@@ -139,7 +154,7 @@ int main() {
 
 	printf("The root found is: \n");
 	printv(x);
-	printf("Here f(x) is\n");
+	printf("Here df(x) is\n");
 	Himmelblau_f(x,df,H);
 	printv(df);
 	printf("Number of function calls: %i\n",function_calls);
@@ -149,7 +164,7 @@ int main() {
 	printf("\n\n");
 
 	printf("B. Quasi Newton with Broyden analytical gradient\n");
-	
+	printf("i)\n");
 	// Rosenbrock
 	function_calls = 0;
 	printf("Testing if the implementation works on Rosenbrock.\n");
@@ -164,7 +179,7 @@ int main() {
 
 	printf("The root found is: \n");
 	printv(x);
-	printf("Here f(x) is\n");
+	printf("Here df(x) is\n");
 	Rosenbrock_f(x,df,H);
 	printv(df);
 	printf("Number of function calls: %i\n",function_calls);
@@ -180,16 +195,82 @@ int main() {
 	printf("xstart is:\n");
 	printv(x);
 
-	newton_minimizationBroyden(Himmelblau_f_Broyden,x,dx,epsilon);	
+	newton_minimization_Broyden(Himmelblau_f_Broyden,x,dx,epsilon);	
 
 	printf("The root found is: \n");
 	printv(x);
-	printf("Here f(x) is\n");
-	Himmelblau_f_B(x,df,H);
+	printf("Here df(x) is\n");
+	Himmelblau_f(x,df,H);
 	printv(df);
 	printf("Number of function calls: %i\n",function_calls);
+	printf("Something very interesting happened when using Broyden:\n"
+			"The minima for Himmelblau found is a different one, but still correct\n"
+			"since Himmelblau has several minima.\n");
+
+	printf("\n");
+	printf("iii) Comparison\n"
+			"If we compare explicit hessian to Broyden, we see that \n"
+			"Explicit Hessian uses many fewer steps than updating without Hessian\n"
+			"This does no nessesarily mean that classical Newton is better\n"
+			"actually possibly broyden is still faster because it doesn't \n"
+			"solve the linear system of equations to find delta x at each step and doesn't\n"
+			"calculate the hessian each time the function is run. \n"
+			"If we compare to the root finding algorithms they take even less steps\n"
+			"and seem to be the most effective overall.\n"
+			"Again however it is difficult to say overall because it might be a parameter issue\n");
+
+	n=3;
+	printf("\n");
+	printf("iv) Testing at fit problem\n");
+	double t[] = {0.23,1.29,2.35,3.41,4.47,5.53,6.59,7.65,8.71,9.77};
+	double y[] = {4.64,3.38,3.01,2.55,2.29,1.67,1.59,1.69,1.38,1.46};
+	double e[] = {0.42,0.37,0.34,0.31,0.29,0.27,0.26,0.25,0.24,0.24};
+	int N = sizeof(t)/sizeof(t[0]);
+
+	gsl_vector* x_fit = gsl_vector_alloc(n);
+	gsl_vector* df_fit = gsl_vector_alloc(n);
+
+	FILE* data = fopen("data.txt","w+");
+	for(int i=0; i<N; i++){
+		fprintf(data,"%g\t%g\t%g\n",t[i],y[i],e[i]);
+	}
+	fclose(data);
+
+	printf("I have found the gradient of the master funciton numerically\n");
+	function_calls = 0;
+	gsl_vector_set(x_fit,0,5);
+	gsl_vector_set(x_fit,1,0);
+	gsl_vector_set(x_fit,2,5);
+	
+	printf("I set dx=%g and epsilon=%g\n",dx, epsilon);
+	printf("xstart is (qualified guess from data):\n");
+	printv(x_fit);
+
+	newton_minimization_Broyden_num_gradient(master_fit,x_fit,dx,epsilon);	
+
+	printf("The root found is: \n");
+	printv(x_fit);
+	printf("Here df(x) is\n");
+	gradient_num(master_fit,x_fit,df_fit,dx);
+	printv(df_fit);
+	printf("Number of function calls: %i\n",function_calls);
+
+	double tstart=0.0, tslut=10.0, deltat=tslut/200;
+	double A=gsl_vector_get(x_fit,0);
+	double B=gsl_vector_get(x_fit,1);
+	double T=gsl_vector_get(x_fit,2);
+	double ft;
+	FILE* fitdata = fopen("fit.txt","w+");
+
+	for(double t=0;t<tslut;t+=deltat){
+		ft=A*exp(-t/T)+B;
+		fprintf(fitdata, "%g\t%g\n",t, ft);
+	}
+	fclose(fitdata);
 	gsl_vector_free(x);
 	gsl_vector_free(df);
+	gsl_vector_free(x_fit);
+	gsl_vector_free(df_fit);
 	gsl_matrix_free(H);		
 
 	return EXIT_SUCCESS;
