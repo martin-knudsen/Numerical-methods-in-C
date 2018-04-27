382cb3ab7313a92cd5fcc04d1f91f8bcc31ff4d5
Unknown <martin.knudsen@protonmail.com>
2018-04-27 09:30:16 +0200

    A done

--- Homeworks/Numerical Methods/Monte Carlo/Monte_Carlo.c
+++ Homeworks/Numerical Methods/Monte Carlo/Monte_Carlo.c
@@ -3,45 +3,52 @@
 #include <stdlib.h>
 #include "Monte_Carlo.h"
 #include <assert.h>
-#define  RND (double )rand()/RANDMAX // just for a random number between 0 and 1
+#define  RND (double )rand()/RAND_MAX // just for a random number between 0 and 1
 
 /* These methods are taken from Dmitri Fedorovs lecture notes 
 
-	This function just */
+	This function just calcuates a random number within the integration limits
+	for each integration variable, meaning each dimension of array x */
 void randomx(int dim,double *a,double *b,double *x){
+	// for each dimension rescale the RND value to be in the interval of the limits for that variable
+	// and assigns them to the x array
 	for(int i=0;i<dim;i++){
 		x[i]=a[i]+RND*(b[i]-a[i]);
 	}
 }
 
+/* This is the actual plain Monte Carlo integrator. 
+It takes in the dimension of the problem, the limits of all the integration variables
+as arrays a and b, the integration function, taking an array x of variables.
+The number of random points N, the result and the error*/
 void plainmc(int dim,double *a,double *b,
 double f(double *x),int N,double *result,double *error){
 	double V=1;
-
+	// Here we set the multidiminsional volume as a multidimensional "rectangle"
+	// containing the integration volume Omega. Just by multiplying with sides of the 
+	// rectangle as big as the integration limits for each integration variable.
 	for(int i=0;i<dim;i++){
-		V*=b[i]*a[i];
-		double sum=0,sum2=0,fx,x[dim];
+		V*=b[i]-a[i];
 	}
+	double sum=0,sum2=0,fx,x[dim];
+	
+	// loop for finding random multi-Dimensional point, getting the 
+	// function value at that point, and calculating the sum of the 
+	// function values and the function values squared for finding the
+	// expectation value of both.
 	for(int i=0;i<N;i++){
 		randomx(dim,a,b,x);
 		fx=f(x);
 		sum+=fx;
 		sum2+=fx*fx;
 	}
-	double avr=sum/N,var=sum2/N*avr*avr;
-
-	*result=avr*V;*error=sqrt(var/N)*V;
+	// Average <f> by dividing with number of points
+	double avr=sum/N;
+	//finding the variance, sigma^2, by using the standard formular (9.4) 
+	double var=sum2/N-avr*avr;
+	// estimate of the integral is just (9.2) 
+	*result=avr*V;
+	// the error is just the squareroot of the variance
+	*error=sqrt(var/N)*V;
 }
 
-/*
-void plainmc2(double f(double *x),double  *a,double* b, N){
-   var randomx = function(a,b) [a[i]+Math.random()*(b[i]-a[i]) for (i in a)];
-   var volume=1; for(var i in a) volume*=b[i]-a[i];
-   var sum=0,sum2=0;
-   for(var i=0;i<N;i++){var fx=f(randomx(a,b)); sum += fx; sum2 += fx*fx}
-   var mean = sum/N;                             // <f_i>
-   var sigma = Math.sqrt(sum2/N - mean*mean);    // sigma² = <(f_i)²> - <f_i>²
-   var SIGMA = sigma/Math.sqrt(N);               // SIGMA² = <Q²> - <Q>²
-   return [mean*volume, SIGMA*volume];
-}
-*/
--- Homeworks/Numerical Methods/Monte Carlo/main.c
+++ Homeworks/Numerical Methods/Monte Carlo/main.c
@@ -9,14 +9,53 @@
 
 int main(void) {
 	// part A 
-
 	printf("A. Plain Monte Carlo integration\n\n");
+	printf("Integrating dr dphi dtheta r^2 from 0 to 1, 0 to 2pi and 0 to pi\n");
 	
+	int dim=3, N=1e6; double a[3]={0,0,0}, b[3]={1,2*M_PI,M_PI},result, error;
+	printf("Number of points: %i\n",N);
+	double f1(double* x){
+		return x[0]*x[0];
+		
+	}
+
+	plainmc(dim,a,b,f1,N,&result,&error);
+
+	printf("Result: %g, correct answer is: %g\n",result,2*M_PI*M_PI/3);
+	printf("Error: %g \n\n",error);
+
+	printf("Integrating dx dy dz dq x*y^2*z^3*q^4 from 0 to 1, 1 to 2 and 2 to 3 and 3 to 4\n");
+	
+	dim=4; N=1e7; double a2[4]={0,1,2,3}, b2[4]={1,2,3,4};
+	printf("Number of points: %i\n",N);
+	double f2(double* x){
+		return x[0]*x[1]*x[1]*x[2]*x[2]*x[2]*x[3]*x[3]*x[3]*x[3];
+	}
+
+	plainmc(dim,a2,b2,f2,N,&result,&error);
+
+	printf("Result: %g, correct answer is: %g\n",result,71071.0/24.0);
+	printf("Error: %g\n\n",error);
+
+	printf("Integrating dx dy dz (1-cos(x)cos(y)cos(z))^-1/pi^3 from 0 to pi, 0 to pi and 0 to pi\n");
+	
+	dim=3; N=1e7; double a3[3]={0,0,0}, b3[3]={M_PI,M_PI,M_PI};
+	printf("Number of points: %i\n",N);
+	double f3(double* x){
+		return 1/(1-cos(x[0])*cos(x[1])*cos(x[2]))/(M_PI*M_PI*M_PI);
+	}
+
+	plainmc(dim,a3,b3,f3,N,&result,&error);
+
+	printf("Result: %g, correct answer is: %7.6f \n",result,1.3932039296856768591842462603255);
+	printf("Error: %g\n\n",error);
+
+
 	// part B
 	printf("B. Check that error behaves as O(1/√N)\n\n");	
 
 	// part C
-	printf("B. 2D adaptive integrator\n\n");
+	printf("C. 2D adaptive integrator\n\n");
 
 
 	return EXIT_SUCCESS;
